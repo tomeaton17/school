e141a1d016632941d0c2a7e538f40f2a2b307849
TomEaton <thomfurpwns@gmail.com>
2016-12-06 15:29:44 +0000

    Added python ui generated by Qt creator. Called from ui.py. Change radioactivedecay.py to be callable when imported.
    Signed-off-by: TomEaton <thomfurpwns@gmail.com>

---
+++ test.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_main(object):
+    def setupUi(self, main):
+        main.setObjectName("main")
+        main.resize(746, 354)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(100)
+        sizePolicy.setHeightForWidth(main.sizePolicy().hasHeightForWidth())
+        main.setSizePolicy(sizePolicy)
+        main.setAutoFillBackground(True)
+        self.centralWidget = QtWidgets.QWidget(main)
+        self.centralWidget.setObjectName("centralWidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 751, 361))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(11, 11, 11, 11)
+        self.gridLayout.setSpacing(6)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.label.setFont(font)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 2)
+        self.pushButton = QtWidgets.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 150))
+        self.pushButton.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 2, 0, 2, 1)
+        self.pushButton_2 = QtWidgets.QPushButton(self.gridLayoutWidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy)
+        self.pushButton_2.setMinimumSize(QtCore.QSize(0, 150))
+        self.pushButton_2.setMaximumSize(QtCore.QSize(300, 16777215))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.gridLayout.addWidget(self.pushButton_2, 2, 1, 2, 1)
+        main.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi(main)
+        QtCore.QMetaObject.connectSlotsByName(main)
+
+    def retranslateUi(self, main):
+        _translate = QtCore.QCoreApplication.translate
+        main.setWindowTitle(_translate("main", "Physics Practice"))
+        self.label.setText(_translate("main", "Physics Practice"))
+        self.pushButton.setText(_translate("main", "Projectile motion"))
+        self.pushButton_2.setText(_translate("main", "Radioactive Decay\n"
+                                                     ""))
