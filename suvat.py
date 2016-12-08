69dc10bd095226a19203832805e2f55977804402
TomEaton <thomfurpwns@gmail.com>
2016-12-08 14:10:20 +0000

    Cleaned up file, not complete yet

--- suvat.py
+++ suvat.py
@@ -2,52 +2,53 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+
 def generate():
     increment = 0.0001  # Time increment used when calculating values
-    initialSpeed = 10
+    initial_speed = 10
     theta = np.radians(70)  # Angle that the direction of launch makes to the horizontal
 
-    xSpeed = initialSpeed * np.cos(theta)  # Using trig to calculate xspeed. Assumes there is no air resistance
-    ySpeed = initialSpeed * np.sin(theta)
+    x_speed = initial_speed * np.cos(theta)  # Using trig to calculate x_speed. Assumes there is no air resistance
+    y_speed = initial_speed * np.sin(theta)
 
-    xOffset = 0  # Initial x position. Can be changed to add an offset which can be in a question
-    yOffset = 0  # Initial y position. Can be changed to add an offset which can be in a question
+    x_offset = 0  # Initial x position. Can be changed to add an offset which can be in a question
+    y_offset = 0  # Initial y position. Can be changed to add an offset which can be in a question
 
-    xPos = []  # X position array
-    yPos = []  # Y position array
+    x_pos = []  # X position array
+    y_pos = []  # Y position array
     time = 0  # Initial time
 
-    xPos.append(xOffset)  # Adding the offset in the first slot of the position array
-    yPos.append(yOffset)  # Adding the offset in the first slot of the position array
+    x_pos.append(x_offset)  # Adding the offset in the first slot of the position array
+    y_pos.append(y_offset)  # Adding the offset in the first slot of the position array
 
     time += increment
-    xPosTemp = xSpeed * time  # Calculating first x value so while loop doesn't fail instantly
-    yPosTemp = (ySpeed * time) + (
-        0.5 * -9.8 * time ** 2)  # Using SUVAT to calculate first y value. This is so that the while loop doesn't fail instantly
+    x_pos_temp = x_speed * time  # Calculating first x value so while loop doesn't fail instantly
+    y_pos_temp = (y_speed * time) + (
+        0.5 * -9.8 * time ** 2)  # Using SUVAT to calculate first y value. This is so that the while loop doesn't fail
 
-    xPos.append(xPosTemp)
-    yPos.append(yPosTemp)
+    x_pos.append(x_pos_temp)
+    y_pos.append(y_pos_temp)
 
-    while (yPosTemp > 0):
-        xPosTemp = xSpeed * time
-        yPosTemp = (ySpeed * time) + (0.5 * -9.8 * time ** 2)
-        xPos.append(xSpeed * time)
-        yPos.append((ySpeed * time) + (0.5 * -9.8 * time ** 2))
+    while y_pos_temp > 0:
+        x_pos_temp = x_speed * time
+        y_pos_temp = (y_speed * time) + (0.5 * -9.8 * time ** 2)
+        x_pos.append(x_speed * time)
+        y_pos.append((y_speed * time) + (0.5 * -9.8 * time ** 2))
         time += increment
 
-    plt.plot(xPos, yPos)
-    plt.axis([0, xPos[len(xPos) - 1], 0, max(yPos) + max(yPos) * .2])
+    plt.plot(x_pos, y_pos)
+    plt.axis([0, x_pos[len(x_pos) - 1], 0, max(y_pos) + max(y_pos) * .2])
     plt.xlabel("X displacement")
     plt.ylabel("Y displacement")
-    index = yPos.index(max(yPos))
-    xValue = xPos[int(index)]
-    plt.annotate("Maximum height: " + str(max(yPos)), (xValue, max(yPos)),
-             (xValue - 0.2 * max(xPos), max(yPos) + max(yPos) * .1), arrowprops=dict(arrowstyle="->"))
+    index = y_pos.index(max(y_pos))
+    x_value = x_pos[int(index)]
+    plt.annotate("Maximum height: " + str(max(y_pos)), (x_value, max(y_pos)),
+                (x_value - 0.2 * max(x_pos), max(y_pos) + max(y_pos) * .1), arrowprops=dict(arrowstyle="->"))
     plt.grid(True)
     plt.savefig('test.png', bbox_inches='tight', pad_inches=0)
     im = Image.open('test.png')
     size = np.asarray(im.size)
-    size = size / 1.4
+    size /= 1.4
     size.tolist()
     im.thumbnail(size)
     im.save('smaller.png')
