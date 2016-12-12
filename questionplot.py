76eb75c360bd277429999db73be49d0a617b12dd
TomEaton <thomfurpwns@gmail.com>
2016-12-12 09:31:35 +0000

    Diagram draw almost completed

--- questionplot.py
+++ questionplot.py
@@ -28,15 +28,22 @@ def y_displacement(y_offset, initial_speed, theta):
         y_pos.append((y_speed * time) + (0.5 * -9.8 * time ** 2) + 12)
         time += increment
     x_distance = max(x_pos)
-    print(x_pos)
-    print(y_pos)
+    print(x_pos, y_pos)
     plt.plot(x_pos, y_pos)
     plt.annotate(s="", xy=(-1, y_offset), xytext=(-1, 0), arrowprops=dict(arrowstyle='<->'))
+    plt.annotate(s="", xy=(0, -1), xytext=(max(x_pos), -1), arrowprops=dict(arrowstyle='<->'))
     plt.plot([0, x_distance], [0, 0], color='k', linestyle='-', linewidth=2)
     plt.plot([0, 0], [0, y_offset], color='k', linestyle='-', linewidth=2)
-    plt.text(-3, y_offset / 2, str(y_offset), style='normal')
-    plt.axis([-5, x_distance, 0, 30])
+    plt.plot([-0, -1], [y_offset, y_offset], color='k', linestyle='-', linewidth=2)
+    plt.plot([0, x_pos[y_pos.index(max(y_pos))]], [y_offset, y_offset], color='k', linestyle='--', linewidth=2)
+    plt.annotate(s="", xy=(x_pos[y_pos.index(max(y_pos))], max(y_pos)),
+                 xytext=(x_pos[y_pos.index(max(y_pos))], y_offset), arrowprops=dict(arrowstyle='<->'))
+    plt.text(-max(x_pos) * 0.08, y_offset / 2, str(y_offset), style='normal')
+    plt.text(max(x_pos) / 2, -max(x_pos) * 0.05, str(max(x_pos)), style='normal')
+    plt.text(x_pos[y_pos.index(max(y_pos))] + 1, ((max(y_pos) - y_offset) / 2) + y_offset, str(max(y_pos) - y_offset),
+             style='normal')
+    plt.axis([-5, x_distance, -5, max(y_pos) + 5])
     plt.axis('off')
     plt.show()
 
-y_displacement(12, 20, 45)
+y_displacement(12, 20, 30)
