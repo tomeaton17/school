60d5107b719f2eb79089658c66aea19856f76161
TomEaton <thomfurpwns@gmail.com>
2017-01-11 19:06:20 +0000

    Added a radioactive gui to python bat file

---
+++ pyuicradioactive.bat
@@ -0,0 +1,3 @@
+IF EXIST C:\Users\TomEaton\PycharmProjects\school\radioactivegui.py del /F C:\Users\TomEaton\PycharmProjects\school\radioactivegui.py 
+
+pyuic5 -o C:\Users\TomqycharmProjects\school\radioactivegui.py C:\Users\TomEaton\Documents\suvat\radioactive.ui
