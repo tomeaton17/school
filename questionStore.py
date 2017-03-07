b525a5109d8c5c79e13a8990f3e98ca598cdc1a4
Tom Eaton <thomfurpwns@gmail.com>
2017-03-07 12:33:00 +0000

    Development nearly complete

--- questionStore.py
+++ questionStore.py
@@ -2,18 +2,22 @@ import random
 import string
 import questionplotclass
 
-
+#Randomized class, that is used to format strings, and return question objects.
+#This is the parent of the RandomizeddFormatter class.
 class Randomized(object):
     def __init__(self):
         self.args = {}
         self.question = None
 
+    #Returns the value of the randomized variable in the string.
     def __getitem__(self, name):
         return RandomizedFormatter(name, self.args)
 
+    #Formats the string, using the overridden method found in the RandomizedFormatter class.
     def format(self, s):
         return string.Formatter().vformat(s, args=(), kwargs=self)
 
+    #Returns the question class depending on the equation type.
     def get_class(self):
         if self.args['equation'] == "findtheta":
             self.question = questionplotclass.ProjectileQuestion(self.args['b'], self.args['a'], random.randint(40, 60))
@@ -28,12 +32,14 @@ class Randomized(object):
             self.question.find_xdistance()
             return self.question
 
-
+#RandomizedFormatter class. Child of Randomized class. Overrides String formatter to allow
+#question formatting.
 class RandomizedFormatter(object):
     def __init__(self, name, args):
         self.name = name
         self.args = args
 
+    #This function overrides the Python string formatter.
     def __format__(self, fmt):
         op, rest = fmt.split(':', 1)
 
@@ -51,6 +57,7 @@ def load(questionType, object):
     lines = [line.rstrip('\n') for line in open(questionType + ".txt")]
     return object.format(random.choice(lines))
 
+#This code only runs if the file is run specifically, not when it is imported.
 if __name__ == '__main__':
     rr = Randomized()
     load("projectilemotionquestions", rr)
